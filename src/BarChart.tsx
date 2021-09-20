47c27cf58a63ac80859267a6438e22768e81324a
My Lucky Day <devs.myluckyday@gmail.com>
2021-09-20 13:57:29 +0200

    Update BarChart.tsx

--- src/BarChart.tsx
+++ src/BarChart.tsx
@@ -78,7 +78,7 @@ class BarChart extends AbstractChart<BarChartProps, BarChartState> {
       const barWidth = 32 * this.getBarPercentage();
       return (
         <Rect
-          onPress={()=>_this.props.onBarPress({value:x,index:i})}
+          onPress={()=>this.props.onBarPress({value:x,index:i})}
           key={Math.random()}
           x={
             paddingRight +
